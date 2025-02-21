3be6ce433b180cf3e11a774b846e2943ac36e867
Quantumania23 <mikepeace981@gmail.com>
2025-02-21 16:27:28 +0300

    update

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,11 +1,36 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+// 	try {
+// 		const conn = await mongoose.connect(process.env.MONGO_URI);
+// 		console.log(`MongoDB connected: ${conn.connection.host}`);
+// 	} catch (error) {
+// 		console.error(`Error connecting to MongoDB: ${error.message}`);
+// 		process.exit(1);
+// 	}
+// };
+
+
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 export const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
-		console.log(`MongoDB connected: ${conn.connection.host}`);
-	} catch (error) {
-		console.error(`Error connecting to MongoDB: ${error.message}`);
-		process.exit(1);
-	}
+  if (!process.env.MONGO_URI) {
+    console.error('MONGO_URI is not defined in the environment variables');
+    process.exit(1);
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
 };
